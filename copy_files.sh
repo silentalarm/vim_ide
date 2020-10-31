f48b936f6631fb051c4a233c36b9fa36d2b7a63d
s1lentalarm <nsilentalarm@gmail.com>
2020-10-31 16:59:32 +0300

    fix .zshrc

--- copy_files.sh
+++ copy_files.sh
@@ -1,9 +1,21 @@
-#!/bin/bash
+#!/bin/zsh
 # ./copy_files.sh
 
 
-cp .vimrc ~/
-cp .zshrc ~/
-cp .ycm_extra_conf.py ~/.vim/bundle/YouCompleteMe/.ycm_extra_conf.py
-cp codedark.vim ~/.vim/bundle/vim-colorschemes/colors
-cp molokai.vim ~/.vim/bundle/vim-colorschemes/colors
+if [ ! -z "$USER" ]
+then
+    echo "USER=`/usr/bin/whoami`" >> ~/.zshrc
+    echo "export USER" >> ~/.zshrc
+fi
+
+mkdir -p ~/.vim/plugin
+
+cp req/stdheader.vim ~/.vim/plugin/
+cp req/.vimrc ~/
+cp req/.zshrc ~/
+cp req/.ycm_extra_conf.py ~/.vim/bundle/YouCompleteMe/.ycm_extra_conf.py
+cp req/codedark.vim ~/.vim/bundle/vim-colorschemes/colors
+cp req/molokai.vim ~/.vim/bundle/vim-colorschemes/colors
+
+source ~/.zshrc
+
